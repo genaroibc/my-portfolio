dc9141d8ef380b078ec38c635015ad95c3a6bfb2
genaroibc <bonavitaismael@gmail.com>
2023-01-11 14:42:27 -0300

    [REFACTOR]: separate Icon types into a single file

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+export type IconName =
+  | "aws"
+  | "blockchain"
+  | "bootstrap"
+  | "css"
+  | "git"
+  | "github"
+  | "html"
+  | "js"
+  | "mongodb"
+  | "nextjs"
+  | "node"
+  | "react"
+  | "ts";
+
+export type IconSize = "tiny" | "small" | "medium" | "large" | "huge";
